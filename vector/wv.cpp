a0c981e290febef54a62011af6058a3fbb123786
wernerwessely <wessely.werner@gmail.com>
2020-02-17 14:42:35 +0100

    Some more vector stuff

--- vector/wv.cpp
+++ vector/wv.cpp
@@ -2,7 +2,39 @@
 
 namespace W
 {
-wv::wv() : _mem(std::make_unique<int[]>(2))
+
+wv::wv()
 {
 }
+
+size_t wv::size() const
+{
+    return _size;
+}
+
+bool wv::empty() const
+{
+    return !_size;
+}
+
+size_t wv::capacity() const
+{
+    return _capacity;
+}
+
+void wv::push(int value)
+{
+    realloc();
+}
+
+void wv::realloc()
+{
+    if (_size >= _capacity / 2)
+    {
+    }
+    else if (_size <= _capacity / 4)
+    {
+    }
+}
+
 } // namespace W
--- vector/wv.hpp
+++ vector/wv.hpp
@@ -8,12 +8,26 @@ namespace W
 class wv
 {
 public:
+    // Ctor.
     wv();
+    // Current number of elements.
+    size_t size() const;
+    // Get if empty.
+    bool empty() const;
+    // Get current capacity.
+    size_t capacity() const;
+    // Append to end of vector.
+    void push(int value);
+    // Get and remove last item.
+    int pop();
 
 private:
     wv(const wv &other);
     wv operator=(const wv &other);
     std::unique_ptr<int[]> _mem;
+    size_t _size = 0;
+    size_t _capacity = 2;
+    void realloc();
 };
 } // namespace W
 
