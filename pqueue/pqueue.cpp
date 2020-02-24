63768473b95cf6befb6a4f9a0c5923afd649bf74
wernerwessely <wessely.werner@gmail.com>
2020-02-24 16:50:58 +0100

    Started pqueue impl

--- pqueue/pqueue.cpp
+++ pqueue/pqueue.cpp
@@ -21,6 +21,37 @@ size_t pqueue::height() const
 void pqueue::push(size_t prio)
 {
     _mem.push_back(prio);
+
+    size_t i = _mem.size() - 1;
+
+    for (size_t p = (i - 1) / 2; i && _mem[i] < _mem[p]; std::swap(_mem[p], _mem[i]), i = p)
+        ;
+}
+
+size_t pqueue::pop()
+{
+    auto l = _mem.size();
+
+    if (!l)
+    {
+        throw std::out_of_range("");
+    }
+
+    auto tmp = _mem[l - 1];
+    _mem.pop_back();
+
+    std::swap(tmp, _mem[0]);
+
+    size_t i = 0;
+
+    while (i < l)
+    {
+        size_t l = i * 2 + 1, r = i * 2 + 2;
+
+        size_t c = l < r ? r : l;
+
+        if ()
+    }
 }
 
 } // namespace W
