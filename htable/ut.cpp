c271b972ab64c582e5a6c8f786105c1d9da0bed5
wernerwessely <wessely.werner@gmail.com>
2020-02-29 19:12:23 +0100

    Use conan

--- htable/ut.cpp
+++ htable/ut.cpp
@@ -1,6 +1,6 @@
 
 #define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+#include <catch2/catch.hpp>
 #include "htable.hpp"
 
 TEST_CASE("Test the add() function.")
--- pqueue/ut.cpp
+++ pqueue/ut.cpp
@@ -1,6 +1,6 @@
 
 #define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+#include <catch2/catch.hpp>
 #include "pqueue.hpp"
 
 TEST_CASE("Test the height() function.")
