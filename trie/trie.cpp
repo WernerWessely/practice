00d87b7aae974ad991074865fca73231e6420ad8
wernerwessely <wessely.werner@gmail.com>
2020-03-06 17:45:01 +0100

    Some more trie

--- trie/trie.cpp
+++ trie/trie.cpp
@@ -15,6 +15,15 @@ void trie::_add(trienp &ch, const std::string &key, size_t index, unsigned val)
     {
         throw std::out_of_range("");
     }
+
+    if (!key[index + 1])
+    {
+        // End of the word.
+        ch->_val = val;
+    }
+    else
+    {
+    }
 }
 
 } // namespace W
