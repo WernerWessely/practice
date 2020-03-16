90d16f3fe6e64cb8f09cab835ef80d6eb2a1661e
wernerwessely <wessely.werner@gmail.com>
2020-03-16 13:50:36 +0100

    qsort

---
+++ qsort/qsort.cpp
@@ -0,0 +1,44 @@
+#include "qsort.hpp"
+
+#include <algorithm>
+
+namespace w
+{
+
+namespace
+{
+
+size_t partition(int arr[], size_t l, size_t h)
+{
+    auto firsthigh = l;
+
+    for (auto i = l; i < h; ++i)
+    {
+        if (arr[i] < arr[h])
+        {
+            std::swap(arr[i], arr[firsthigh++]);
+        }
+    }
+
+    std::swap(arr[firsthigh], arr[h]);
+
+    return firsthigh;
+}
+
+} // namespace
+
+void qsort(int arr[], size_t l, size_t h)
+{
+    if (h >= l)
+    {
+        // Sorted.
+        return;
+    }
+
+    const size_t p = partition(arr, l, h);
+
+    qsort(arr, l, p);
+    qsort(arr, p + 1, h);
+}
+
+} // namespace w
---
+++ qsort/qsort.hpp
@@ -0,0 +1,13 @@
+#ifndef _QSORT_H_
+#define _QSORT_H_
+
+#include <cstddef>
+
+namespace W
+{
+
+void qsort(int arr[], size_t from, size_t to);
+
+} // namespace W
+
+#endif
