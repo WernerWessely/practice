9301c8d3c25c08987ae5311a4897f30e67874e82
wernerwessely <wessely.werner@gmail.com>
2020-05-06 08:57:52 +0200

    cont. msort rewrite

--- msort/msort.cpp
+++ msort/msort.cpp
@@ -1,1 +1,89 @@
 #include "msort.hpp"
+
+namespace w
+{
+
+    namespace
+    {
+        int len(node *l)
+        {
+            int n = 0;
+
+            for (; l; l = l->_next, ++n)
+                ;
+
+            return n;
+        }
+
+        node *split(node *l)
+        {
+            const int n = len(l);
+
+            if (n > 1)
+            {
+                node *cur = l;
+
+                for (int i = 0;; cur = cur->_next, ++i)
+                {
+                    if (i == n / 2)
+                    {
+                        // Detach second half of list and return
+                        node *ret = cur->_next;
+                        cur->_next = nullptr;
+                        return ret;
+                    }
+                }
+            }
+
+            return nullptr;
+        }
+
+        void append(node *&start, node *&end, node *l)
+        {
+            if (!start)
+            {
+                start = l;
+            }
+            else
+            {
+                end->_next = l;
+            }
+
+            end = l;
+        }
+
+        node *merge(node *l1, node *l2)
+        {
+            node *ret = nullptr, *end = nullptr;
+
+            while (l1 && l2)
+            {
+                node *&next = l1->_val <= l2->_val ? l1 : l2;
+
+                append(ret, end, next);
+                next = next->_next;
+            }
+
+            append(ret, end, l1 ? l1 : l2);
+
+            return ret;
+        }
+
+    } // namespace
+
+    node *msort(node *l)
+    {
+        node *first = l, *second = split(first);
+
+        if (!second)
+        {
+            // Base case
+            return first;
+        }
+
+        msort(first);
+        msort(second);
+
+        return merge(first, second);
+    }
+} // namespace w
--- msort/ut.cpp
+++ msort/ut.cpp
@@ -2,3 +2,77 @@
 
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <initializer_list>
+#include <vector>
+
+namespace w
+{
+    typedef std::vector<int> ser;
+
+    // A sorted list for convenience
+    class slist
+    {
+    public:
+        explicit slist(std::initializer_list<int> init) : _root(nullptr)
+        {
+            for (auto v : init)
+            {
+                (_root ? _root->_next : _root) = new w::node(v);
+            }
+
+            w::msort(_root);
+        }
+
+        ~slist()
+        {
+            while (_root)
+            {
+                w::node *del = _root;
+                _root = _root->_next;
+                delete del;
+            }
+        }
+
+        slist &operator=(const slist &other) = delete;
+        slist(const slist &other) = delete;
+
+        ser serial() const
+        {
+            std::vector<int> ret;
+
+            for (w::node *cur = _root; cur; cur = cur->_next)
+            {
+                ret.push_back(cur->_val);
+            }
+
+            return ret;
+        }
+
+    private:
+        w::node *_root;
+    };
+} // namespace w
+
+TEST_CASE("test msort() with list.")
+{
+    SECTION("empty")
+    {
+        w::slist l{};
+
+        REQUIRE(l.serial() == w::ser{});
+    }
+
+    SECTION("1")
+    {
+        w::slist l{1};
+
+        REQUIRE(l.serial() == w::ser{1});
+    }
+
+    SECTION("2")
+    {
+        w::slist l{2, 1};
+
+        REQUIRE(l.serial() == w::ser{1, 2});
+    }
+}
